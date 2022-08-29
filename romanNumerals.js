5d1d15ce8d02ad4578ec47ab0ad0661661974f5a
Angeleah <a.hoeppner@yahoo.ca>
2022-08-28 20:23:53 -0700

    Solved Roman Numeral Problem

--- romanNumerals.js
+++ romanNumerals.js
@@ -7,9 +7,35 @@
 let ex1 = "III";
 let ex2 = "LVIII";
 let ex3 = "MCMXCIV";
+let ex4 = "I";
+let ex5 = "IX";
+let ex6 = "IV";
+let ex7 = "XL";
 
-function toInt(roman) {
-    console.log(roman);
+const dict = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
 }
 
-export default toInt;
+function toInt(roman) {
+    let sum = 0;
+    let arr = roman.split("");
+    for (let i = 0; i < arr.length; i++) {
+        if ((arr[i] === "I" && (arr[i + 1] === "V" || arr[i + 1] === "X")) ||
+            (arr[i] === "X" && (arr[i + 1] === "L" || arr[i + 1] === "C")) ||
+            (arr[i] === "C" && (arr[i + 1] === "D" || arr[i + 1] === "M"))) {
+
+            sum -= dict[arr[i]];
+        } else {
+            sum += dict[arr[i]];
+        }
+    }
+    console.log(`The roman numeral ${roman} is equivalent to the integer ${sum}`);
+}
+
+toInt(ex7);
