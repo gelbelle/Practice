1e6ee8932dafc1dfa77abcb77ee7dd57e117148a
Angeleah <a.hoeppner@yahoo.ca>
2022-08-29 20:11:33 -0700

    Working on matrix solution

--- twoSums.js
+++ twoSums.js
@@ -20,6 +20,12 @@ let ex2 = {
     target: 24
 }
 
+let ex3 = {
+    nums1: [7, 4, 1, 10],
+    nums2: [4, 5, 8, 7],
+    target: 14
+}
+
 //****Brute force solution O(n^2)****
 function twoSumsBruteForce(nums1, nums2, target) {
     console.log({ nums1, nums2, target });
@@ -40,7 +46,6 @@ console.log("Brute force");
 console.log(twoSumsBruteForce(ex2.nums1, ex2.nums2, ex2.target));
 
 //****Improved version O(n)****
-//Set.has() is fasther than array.includes()
 
 function twoSumsBetter(nums1, nums2, target) {
     console.log({ nums1, nums2, target });
@@ -61,3 +66,55 @@ function twoSumsBetter(nums1, nums2, target) {
 console.log(`
 Better`);
 console.log(twoSumsBetter(ex1.nums1, ex1.nums2, ex1.target));
+
+//****Matrix Solution****
+function sumMatrix(nums1, nums2, target) {
+    nums1.sort((a, b) => a - b);
+    nums2.sort((a, b) => a - b);
+    let mid = Math.round(nums1.length / 2);
+    console.log({ nums1, nums2, target, mid });
+
+
+    let numbers = [];
+
+    let sum = null;
+
+    while (sum !== target && !isNaN(sum)) {
+        let index1 = 0;
+        let index2 = nums2.length - 1;
+        let num1 = nums1.length > 2 ? nums1[index1] : nums1[0];
+        let num2 = nums2.length > 2 ? nums2[index2] : nums2[0];
+
+        sum = nums1[index1] + nums2[index2];
+        console.log({ sum, num1, num2 });
+
+
+        if (sum > target) {
+            console.log(`${sum} > ${target}`);
+            nums1 = nums1.slice(0, index1);
+            nums2 = nums2.slice(0, index2);
+            console.log({ index1, index2 });
+            console.log({ nums1, nums2, num1, num2 });
+        } else {
+            console.log(`${sum} < ${target}`);
+
+            nums1 = nums1.slice(index1 + 1, nums1.length);
+            nums2 = nums2.slice(index2 + 1, nums2.length);
+            console.log({ index1, index2 });
+            console.log({ nums1, nums2, num1, num2 });
+
+        }
+        numbers.push(nums1[index1], nums2[index2]);
+    }
+
+    return {
+        "First number": numbers[0],
+        "Second number": numbers[1],
+        "Target": target
+    };
+
+}
+
+console.log(`
+Matrix Solution`);
+console.log(sumMatrix(ex1.nums1, ex1.nums2, ex1.target));
