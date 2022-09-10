29e2083c323857983fd5b8975bfd0cc2ace7851f
Angeleah <a.hoeppner@yahoo.ca>
2022-09-10 10:58:04 -0700

    Exceeds time but works.

--- trickyDivide.js
+++ trickyDivide.js
@@ -11,27 +11,59 @@ let ex1 = [10, 3];
 let ex2 = [7, -3];
 let ex3 = [1, 1];
 let ex4 = [-1, 1];
-let ex5 = [7, -3];
+let ex5 = [-2147483648, 1];
 let ex6 = [-1, -1];
 let ex7 = [-2147483648, -1];
+let ex8 = [2147483647, 1];
 
+//Takes too long, but seems to work. Solution available if desired.
 function divide(dividend, divisor) {
+    console.log({ dividend, divisor });
     let count = 0;
+    let dividendNeg = isNegative(dividend);
+    console.log({ dividendNeg });
+    let divisorNeg = isNegative(divisor);
+    console.log({ divisorNeg });
+    let isNeg = dividendNeg !== divisorNeg;
 
-    let divNeg = ((dividend >= 0 && divisor > 0) || (dividend < 0 && divisor < 0)) ? true : false;
+    console.log({ isNeg });
 
-    if (divNeg && (dividend > Math.pow(2, 31) - 1) && (divisor === 1 || divisor === -1)) return (Math.pow(2, 31) - 1);
-    if (!divNeg && (dividend <= Math.pow(-2, 31)) && (divisor === 1 || divisor === -1)) {
-        return Math.pow(-2, 31);
-    } else if (divNeg && (dividend <= Math.pow(-2, 31)) && (divisor === 1 || divisor === -1)) {
-        return Math.pow(2, 31) - 1;
+    if (divideByOne(divisor) && outsideBounds(dividend, divisor)) {
+        console.log(`Divide by ${divisor}`);
+        if (isNeg) {
+            console.log({ isNeg });
+            if (dividend >= Math.pow(2, 31) - 1) {
+                return (Math.pow(2, 31)) - 1;
+            } else if (dividend <= Math.pow(-2, 31)) {
+                return Math.pow(-2, 31);
+            }
+        } else if ((dividend >= Math.pow(2, 31) - 1) || (dividend <= Math.pow(-2, 31))) {
+            console.log({ isNeg });
+            return Math.pow(2, 31) - 1;
+        }
+    } else {
+        console.log("While");
+        while (Math.abs(dividend) >= Math.abs(divisor)) {
+            count++;
+            console.log({ count });
+            dividend = (dividend > 0) ? dividend - Math.abs(divisor) : dividend + Math.abs(divisor);
+            console.log({ dividend });
+        }
     }
-
-    while (Math.abs(dividend) >= Math.abs(divisor)) {
-        count++;
-        dividend = (dividend > 0) ? dividend - Math.abs(divisor) : dividend + Math.abs(divisor);
-    }
-
-    return (divNeg) ? count : -count;
+    return isNeg ? -count : count;
 }
-console.log(divide(ex4[0], ex4[1]));
+
+function outsideBounds(num1, num2) {
+    return (num1 >= Math.pow(2, 31) - 1) ||
+        (num1 <= Math.pow(-2, 31));
+}
+
+function isNegative(num) {
+    return num < 0 ? true : false;
+}
+
+function divideByOne(num) {
+    return num === 1 || num === -1;
+}
+
+console.log(divide(ex7[0], ex7[1]));
