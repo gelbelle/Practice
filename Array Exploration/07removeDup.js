7a06e2098ffa8b693179088d03bfb4291a5b03bf
Angeleah <a.hoeppner@yahoo.ca>
2022-10-07 19:52:20 -0700

    Updated with inplace method

--- Array Exploration/07removeDup.js
+++ Array Exploration/07removeDup.js
@@ -20,7 +20,22 @@ function removeDup(nums) {
     return nums;
 }
 
+//Suggested function from tutorial on Leet. Much faster, but only slightly less memory usage
+function removeDup2(nums) {
+    if (nums === null) return 0;
+
+    let pntr = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] != nums[i - 1]) {
+            nums[pntr] = nums[i]; //If current != next then current = current
+            pntr++;
+        }
+    }
+    return pntr;
+}
+
 let ex1 = [1, 1, 2]; // [1,2]
 let ex2 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]; //[0,1,2,3,4]
 
 console.log(removeDup(ex2));
+console.log(removeDup2(ex2));
